9049fcbc5e7364cfd7f7815c69f5138a8f4dc218
MINOR Thomas <mthomas.pro@outlook.fr>
2022-07-19 18:24:46 +0200

    v0.3.0: responsive refactoring

--- scripts/utils/filterBtnZoom.js
+++ scripts/utils/filterBtnZoom.js
@@ -6,61 +6,44 @@ function btnZoom() {
     filterIngredientsBtn.addEventListener("click", event => {
         event.preventDefault();
 
-        if(filterIngredientsBtn.getAttribute("active") === "false") {
-            filterIngredientsBtn.setAttribute("active", "true")
+        filterIngredientsBtn.setAttribute("active", "true")
 
-            filterIngredientsBtn.style.width = "12em";
+        filterIngredientsBtn.style.width = "12em";
 
-            filterToolsBtn.setAttribute("active", "false")
-            filterDevicesBtn.setAttribute("active", "false")
+        filterToolsBtn.setAttribute("active", "false")
+        filterDevicesBtn.setAttribute("active", "false")
 
-            filterDevicesBtn.style.width = "10em";
-            filterToolsBtn.style.width = "10em";
-        } else {
-            filterIngredientsBtn.setAttribute("active", "false")
-
-            filterIngredientsBtn.style.width = "10em";
-        }
+        filterDevicesBtn.style.width = "10em";
+        filterToolsBtn.style.width = "10em";
     })
 
     filterDevicesBtn.addEventListener("click", event => {
         event.preventDefault();
 
-        if(filterDevicesBtn.getAttribute("active") === "false") {
-            filterDevicesBtn.setAttribute("active", "true")
+        filterDevicesBtn.setAttribute("active", "true")
 
-            filterDevicesBtn.style.width = "12em";
+        filterDevicesBtn.style.width = "12em";
 
-            filterIngredientsBtn.setAttribute("active", "false")
-            filterToolsBtn.setAttribute("active", "false")
+        filterIngredientsBtn.setAttribute("active", "false")
+        filterToolsBtn.setAttribute("active", "false")
 
-            filterIngredientsBtn.style.width = "10em";
-            filterToolsBtn.style.width = "10em";
-        } else {
-            filterDevicesBtn.setAttribute("active", "false")
+        filterIngredientsBtn.style.width = "10em";
+        filterToolsBtn.style.width = "10em";
 
-            filterDevicesBtn.style.width = "10em";
-        }
     })
 
     filterToolsBtn.addEventListener("click", event => {
         event.preventDefault();
 
-        if(filterToolsBtn.getAttribute("active") === "false") {
-            filterToolsBtn.setAttribute("active", "true")
+        filterToolsBtn.setAttribute("active", "true")
 
-            filterToolsBtn.style.width = "12em";
+        filterToolsBtn.style.width = "12em";
 
-            filterIngredientsBtn.setAttribute("active", "false")
-            filterDevicesBtn.setAttribute("active", "false")
+        filterIngredientsBtn.setAttribute("active", "false")
+        filterDevicesBtn.setAttribute("active", "false")
 
-            filterDevicesBtn.style.width = "10em";
-            filterIngredientsBtn.style.width = "10em";
-        } else {
-            filterToolsBtn.setAttribute("active", "false")
-
-            filterToolsBtn.style.width = "10em";
-        }
+        filterDevicesBtn.style.width = "10em";
+        filterIngredientsBtn.style.width = "10em";
     })
 }
 
