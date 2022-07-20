a4a9def0ff9ac85c0060d49fc98736a0682d6332
MINOR Thomas <mthomas.pro@outlook.fr>
2022-07-20 16:34:26 +0200

    v0.3.1: add text in recipe card & style

--- scripts/utils/filterBtnZoom.js
+++ scripts/utils/filterBtnZoom.js
@@ -2,6 +2,10 @@ const filterIngredientsBtn = document.querySelector("#filter_ingredients");
 const filterDevicesBtn = document.querySelector("#filter_devices");
 const filterToolsBtn = document.querySelector("#filter_tools");
 
+const filterIngredientsImg = document.querySelector("#filter_ingredients img");
+const filterDevicesImg = document.querySelector("#filter_devices img");
+const filterToolsImg = document.querySelector("#filter_tools img");
+
 function btnZoom() {
     filterIngredientsBtn.addEventListener("click", event => {
         event.preventDefault();
@@ -15,6 +19,10 @@ function btnZoom() {
 
         filterDevicesBtn.style.width = "10em";
         filterToolsBtn.style.width = "10em";
+
+        filterIngredientsImg.style.transform = "rotate(180deg)";
+        filterDevicesImg.style.transform = "rotate(0deg)";
+        filterToolsImg.style.transform = "rotate(0deg)";
     })
 
     filterDevicesBtn.addEventListener("click", event => {
@@ -30,6 +38,9 @@ function btnZoom() {
         filterIngredientsBtn.style.width = "10em";
         filterToolsBtn.style.width = "10em";
 
+        filterIngredientsImg.style.transform = "rotate(0deg)";
+        filterDevicesImg.style.transform = "rotate(180deg)";
+        filterToolsImg.style.transform = "rotate(0deg)";
     })
 
     filterToolsBtn.addEventListener("click", event => {
@@ -44,6 +55,10 @@ function btnZoom() {
 
         filterDevicesBtn.style.width = "10em";
         filterIngredientsBtn.style.width = "10em";
+
+        filterIngredientsImg.style.transform = "rotate(0deg)";
+        filterDevicesImg.style.transform = "rotate(0deg)";
+        filterToolsImg.style.transform = "rotate(180deg)";
     })
 }
 
