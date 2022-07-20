4b8f270dfd37782495c786bac5931472c6545bcf
MINOR Thomas <minorthomas1@gmail.com>
2022-07-20 18:54:40 +0200

    v0.3.2: create template for create and display card & create async funct for display all recipes

--- scripts/constructor/Recipes.js
+++ scripts/constructor/Recipes.js
@@ -1,6 +1,8 @@
 class Recipe {
+
     constructor(data) {
         this._id = data.id;
+        this._name = data.name;
         this._servings = data.servings;
         this._time = data.time;
         this._description = data.description;
@@ -9,6 +11,26 @@ class Recipe {
     }
 
     templateDisplayRecipe() {
-        return `Test`
+        return `
+            <article>
+                <div class="card_header">
+                    <img src="assets/icons/no_image_available.svg" alt="Pas d'image" />
+                </div>
+                <div class="card_footer">
+                    <div class="card_footer_infos">
+                        <h2>${this._name}</h2>
+                        <p>${this._time} min</p>
+                    </div>
+                    <div class="card_footer_recipe">
+                        <div class="card_footer_recipe_left">
+                            <p>test</p>
+                        </div>
+                        <div class="card_footer_recipe_right">
+                            <p>${this._description}</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        `
     }
 }
---
+++ scripts/pages/recipes.js
@@ -0,0 +1,13 @@
+async function displayRecipesCard() {
+    const { recipes } = await getRecipes();
+
+    const cardsSection = document.querySelector("#cards_section");
+
+    recipes.forEach(recipe => {
+        const createRecipeCard = new Recipe(recipe);
+
+        cardsSection.innerHTML += createRecipeCard.templateDisplayRecipe();
+    });
+}
+
+displayRecipesCard();
